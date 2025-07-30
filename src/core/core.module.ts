9551fe9b5bc0b92e3989901dcc1f45ce0b260122
ReynerWL <reynerwl2018@gmail.com>
2025-07-30 14:27:45 +0700

    Feat: Repo

---
+++ src/core/core.module.ts
@@ -0,0 +1,16 @@
+import { Global, Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { JwtStrategy } from './jwt.strategy';
+
+@Global()
+@Module({
+  imports: [
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+  ],
+  exports: [HttpModule, ],
+  providers: [JwtStrategy, ],
+})
+export class CoreModule {}
